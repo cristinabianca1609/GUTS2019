fadc7b9bb6b4d6b8a841f9d8ff9273cdbcb95637
Patsonical <patryk.kaczmarczyk.mail@gmail.com>
2019-10-19 18:18:37 +0100

    Added date parsing to the ocr utility

--- food_waste_project/food_waste_management_app/ocr.py
+++ food_waste_project/food_waste_management_app/ocr.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, re
 
 def api_request(filename, apikey, language='eng'):
     url = 'https://api.ocr.space/parse/image'
@@ -10,10 +10,14 @@ def api_request(filename, apikey, language='eng'):
         r = requests.post(url, files={filename: f}, data=params)
         return r.content.decode()
 
-def parse_response(raw):
-    text = json.loads(raw)["ParsedResults"][0]["ParsedText"]
-    print(text)
-    return 0
+def date_segment_valid(seg):
+    seg = seg.upper()
 
-filename = "/home/patsonical/Documents/DoYouHaveTheGUTS2019/ocrTesting/imgs/expiry02_mini.jpg"
-parse_response(api_request(filename, apikey='bf9a07b67388957'))
+def parse_response(raw):
+    text  = json.loads(raw)["ParsedResults"][0]["ParsedText"]
+    regex = re.findall(r'\b(0[1-9]|[1-2][0-9]|3[0-1]|[A-Za-z]{3,9}|20\d{2})\b', text)
+    date  = dateparser.parse(" ".join(regex.split()))
+    return date
+
+filename = "../static/images/expiry02_mini.jpg"
+print(parse_response(api_request(filename, apikey='bf9a07b67388957')))
