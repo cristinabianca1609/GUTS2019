d80feba4c16848568ac685bd7774abb2c39d6ca8
Cristina <cristina.bianca1609@gmail.com>
2019-10-19 03:30:29 +0100

    added image to static; linked image to index.html; added comment in views

--- food_waste_project/food_waste_management_app/views.py
+++ food_waste_project/food_waste_management_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Here you import any models that we will need to use
+
 # Create your views here.
 def index(request):
     return render(request, 'food_waste_management_app/index.html') #path from templates
