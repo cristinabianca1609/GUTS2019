6fe41518c64f445ba6578bd70c154b22336ac143
Cristina <cristina.bianca1609@gmail.com>
2019-10-19 04:33:51 +0100

    created superuser

--- food_waste_project/food_waste_management_app/admin.py
+++ food_waste_project/food_waste_management_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+#from food_waste_management_app.models import model1,model2
 
 # Register your models here.
--- food_waste_project/food_waste_management_app/views.py
+++ food_waste_project/food_waste_management_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 #Here you import any models that we will need to use
+#from food_waste_management_app.models import 
 
 # Create your views here.
 def index(request):
