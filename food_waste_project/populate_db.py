3f3beca7ac362e78760763cb4ea1dbb0329691e5
Lenka Vincenova <lenkav@sanezoo.com>
2019-10-19 19:24:58 +0100

    db population and barcode csv

--- food_waste_project/populate_db.py
+++ food_waste_project/populate_db.py
@@ -9,13 +9,16 @@ import django
 django.setup()
 
 # from food_waste_project.food_waste_management_app.models import User, Barcode, UserProduct
-from food_waste_management_app.models import *
+from food_waste_management_app.models import User,UserProduct, Barcode
+User.objects.all().delete()
+UserProduct.objects.all().delete()
+Barcode.objects.all().delete()
 
 
-def add_user(d):
-    user = User()
-    user.new_user(d)
-    user.save()
+# def add_user(d):
+#     user = User()
+#     user.new_user(d)
+#     user.save()
 
 
 def add_barcode(d):
@@ -27,18 +30,19 @@ def add_barcode(d):
 def add_product(d):
     p = UserProduct()
     p.add_product(d)
+    # print (p)
     p.save()
 
 
-user1 = {'username': 'lenka', 'password': 'a'}
-user2 = {'username': 'cristina', 'password': 'a'}
-user3 = {'username': 'patrick', 'password': 'a'}
+# user1 = {'username': 'lenka', 'password': 'a'}
+# user2 = {'username': 'cristina', 'password': 'a'}
+# user3 = {'username': 'patrick', 'password': 'a'}
 
-b1 = {'barcode_no': 56353635632, 'name': 'sticks'}
-b2 = {'barcode_no': 643, 'name': 'trees'}
+# b1 = {'barcode_no': 56353687530005632, 'name': 'sticks'}
+# b2 = {'barcode_no': 6436554098776, 'name': 'trees'}
 
-p1 = {'user_id': 1, 'barcode_no': 7622210863362, 'exp_date': datetime.datetime.utcnow() + datetime.timedelta(days=3)}
-p2 = {'user_id': 1, 'barcode_no': 5000168180083, 'exp_date': datetime.datetime.utcnow() + datetime.timedelta(days=3)}
+p1 = {'product_name':'cheese','barcode_no': 7622210863362, 'exp_date': datetime.datetime.utcnow() + datetime.timedelta(days=3)}
+p2 = {'product_name':'bread','barcode_no': 5000168180083, 'exp_date': datetime.datetime.utcnow() + datetime.timedelta(days=3)}
 
 #add_user(user1)
 #add_user(user2)
@@ -52,17 +56,22 @@ p2 = {'user_id': 1, 'barcode_no': 5000168180083, 'exp_date': datetime.datetime.u
 
 
 def runner():
-    #add_user(user1)
-    #add_user(user2)
-    #add_user(user3)
+    with open('generalized_data.csv', 'r') as f:
+        for line in f.readlines()[1:]:
+            full_line = line.split(';')
+            add_barcode({'barcode_no': full_line[0],
+                        'name': full_line[2],
+                        'url':full_line[1]
+                             })
 
-    add_barcode(b1)
-    add_barcode(b2)
-
-    #add_product(p1)
-    #add_product(p2)
+    add_product(p1)
+    add_product(p2)
 
 
 if __name__ == '__main__':
     print("populating script!")
     runner()
+
+
+
+
