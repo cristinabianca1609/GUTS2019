fed16f9b9bf2b2cd2f802f7abf05717f53514a44
Cristina <cristina.bianca1609@gmail.com>
2019-10-20 09:18:23 +0100

    fixed bugs in index.html

--- food_waste_project/food_waste_management_app/admin.py
+++ food_waste_project/food_waste_management_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from food_waste_management_app.models import UserProfileInfo
+from food_waste_management_app.models import UserProfileInfo, Barcode, UserProduct
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
+admin.site.register(Barcode)
+admin.site.register(UserProduct)
--- food_waste_project/populate_db.py
+++ food_waste_project/populate_db.py
@@ -45,6 +45,7 @@ u1 = {'username': 'lenka', 'password':'a'}
 u2 = {'username': 'cristina', 'password':'a'}
 u3 = {'username': 'patryk', 'password':'a'}
 
+<<<<<<< Updated upstream
 
 def populate_barcode():
     with open('generalized_data.csv', 'r') as f:
@@ -90,6 +91,86 @@ def runner():
     populate_barcode()
     # populate_users()
     populate_user_product()
+=======
+add_barcode(b1)
+add_barcode(b2)
+
+add_product(p1)
+add_product(p2)
+result =  (Barcode.objects.filter(barcode_no = '0000000000017').values('product_name'))
+print (result)
+
+def list_user_products(id):
+    result = UserProduct.objects.all().filter(user_id = id)
+    product_exp = dict()
+    for i in result:
+
+        name = Barcode.objects.filter(barcode_id = i.barcode_id)#.values('product_name')
+        product_exp[i.user_product_id] = [name[0].product_name.strip('\n'), i.exp_date]
+
+    return product_exp #dictinary - list value
+
+def get_user_id(username):
+    result = User.objects.filter(username = username)[0]
+    return result.id        #user_id
+def create_barcode(new_bar_dict):
+    new_bar = Barcode()
+    new_bar.new_barcode(new_bar_dict)
+
+    new_bar.save()
+
+def add_user_product(username):
+    product_dict = dict()
+    # barcode_info = scan_barcode()
+    barcode_info = {'barcode': '123123', 'type': 'EAN13'}
+    barcode = barcode_info['barcode']
+    #     is in DB?
+    try:
+        # //TODO: implement the OCR
+        product_dict['exp_date'] = datetime.datetime.utcnow()+datetime.timedelta(days=3)
+        result = Barcode.objects.filter(barcode_no= barcode)
+        product_dict['barcode_no'] = result[0].barcode_no
+
+        new_product = UserProduct()
+        new_product.add_product(product_dict, username)
+        # user = User.objects.filter(username = username)
+        # user.barcode_no = barcode
+        new_product.save()
+        result = 'done'
+    except:
+        result = 'does not exist'
+
+    return result
+
+
+nb = {'barcode_no': '0987654576',
+      'name': 'presnidavka'}
+
+
+def runner():
+    try:
+        with open('generalized_data.csv', 'r') as f:
+            for line in f.readlines()[1:]:
+                full_line = line.split(';')
+                add_barcode({'barcode_no': full_line[0],
+                            'name': full_line[2],
+                            'url':full_line[1]
+                                 })
+    except:
+        pass
+
+    add_product(p1, 'lenka')
+    add_product(p2, 'cristina')
+    add_product(p3, 'lenka')
+
+    list_user_products(get_user_id('lenka'))
+    get_user_id('lenka')
+
+
+    add_user_product('lenka')
+    pass
+
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     print("populating script!")
